e0dceee95c0845ec70ac856e79cf50855d32a31d
Quros7 <loparev.official@yandex.ru>
2024-01-02 04:01:10 +0500

    download (old script)
    logic is correct but with the old GraduateWorkFormatter script

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*- 
-#import sys
-#sys.path.append("../GraduateWorkFormatter")
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from app import app
-#from app import GraduateWorkFormatter
 import os
+import time
+#from app import GraduateWorkFormatter
 from GraduateWorkFormatter import formatter
-#from formatter import Edit
 
 # UPLOAD_FOLDER = os.path.join(app.instance_path, 'uploads')
 # os.makedirs(os.path.join(app.instance_path, 'uploads'), exist_ok=True)
@@ -29,17 +27,21 @@ def upload_file():
     file = request.files['file']
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
-        # Here you should save the file
+        timetemp = str(time.time()).split(".")
+        coded_filename = timetemp[0]+timetemp[1]+"$"+filename
+        # Here we save the file with coded name
         # file.save(path_to_save_file)
-        # РАБОТАЕТ file.save(os.path.join(app.instance_path, 'uploads', filename))
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], coded_filename)
         file.save(file_path)
         flash('Файл ({}) был успешно загружен на сервер'.format(filename))
-        # formatter(file_path) не работает
-        # exec("python test");
-        formatter.Edit(file_path, filename);
+        formatter.Edit(file_path, coded_filename);
         flash('Файл ({}) был успешно отредактирован'.format(filename))
-        return redirect(url_for('index'))
+        ed_filename = "edited_" + filename
+        return render_template('download.html', coded_name="edited_"+coded_filename, name=ed_filename, title='Download')
     
     flash('Ошибка при загрузке файла ({})'.format(filename))
     return redirect(url_for('index'))
+
+@app.route('/download/<name>+<coded_name>')
+def download_file(name, coded_name):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], coded_name, download_name=name, as_attachment=True)
